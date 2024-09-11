3c1256d8a6b3e802d299a0230738b7e6e413c563
WupeiGoogle <153770911+WupeiGoogle@users.noreply.github.com>
2024-09-10 22:11:44 -0700

    Update

--- Package.swift
+++ Package.swift
@@ -17,25 +17,29 @@
 import PackageDescription
 
 let package = Package(
-  name: "GooglePlacesSwift", platforms: [.iOS(.v15)],
-  products: [.library(name: "GooglePlacesSwift", targets: ["GooglePlacesSwiftTarget"])],
-  dependencies: [.package(url: "https://github.com/googlemaps/ios-places-sdk", from: "9.1.0")],
+  name: "GooglePlacesSwiftPreivew", platforms: [.iOS(.v15)],
+  products: [
+    .library(name: "GooglePlacesSwiftPreivew", targets: ["GooglePlacesSwiftPreivew"]),
+    .library(name: "GooglePlacesSwift", targets: ["GooglePlacesSwiftTarget"]),
+    .library(name: "GooglePlacesObjc", targets: ["GooglePlacesObjcTarget"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/googlemaps/ios-places-swift-sdk", from: "0.2.0"),
+    .package(url: "https://github.com/googlemaps/ios-places-sdk", from: "9.1.0"),
+  ],
   targets: [
-    .binaryTarget(
-      name: "GooglePlacesSwift",
-      url: "https://dl.google.com/geosdk/swiftpm/0.2.0/google_places_swift.xcframework.zip",
-      checksum: "0b1daed4bff0d629fe9f939577b16a985ccbb8af9df42d5682ff09a22cf619b4"
-    ),
+    .target(name: "GooglePlacesSwiftPreivew"),
     .target(
       name: "GooglePlacesSwiftTarget",
       dependencies: [
         "GooglePlacesSwift",
-        .product(name: "GooglePlaces", package: "ios-places-sdk"),
-      ],
-      path: "PlacesSwift",
-      sources: ["Empty.swift"],
-      resources: [.copy("Resources/GooglePlacesSwiftResources/GooglePlacesSwift.bundle")],
-      publicHeadersPath: "Sources"
+      ]
     ),
+    .target(
+      name: "GooglePlacesObjcTarget",
+      dependencies: [
+        "GooglePlaces",
+      ]
+    )
   ]
 )
